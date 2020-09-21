f4e624a1c626cc2b5b74ab2d14397215d3c42943
Alejo30 <43772749+Alejo30@users.noreply.github.com>
2020-09-21 01:02:01 -0500

    un cambio menor

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PersonaControllerService } from 'src/app/core/Backend';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private perSrv: PersonaControllerService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  register(event: Event){
+    event.preventDefault();
+  }
+
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      
+    })
   }
 
 }
