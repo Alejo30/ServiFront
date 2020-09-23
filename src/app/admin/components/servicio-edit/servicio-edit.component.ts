be76dec0de2d6bd150fc3180ddb2264e74308e9f
Alejo30 <43772749+Alejo30@users.noreply.github.com>
2020-09-23 17:41:11 -0500

    mas cambios

--- src/app/admin/components/servicio-edit/servicio-edit.component.ts
+++ src/app/admin/components/servicio-edit/servicio-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ServicioControllerService } from 'src/app/core/Backend';
 
 @Component({
   selector: 'app-servicio-edit',
@@ -7,9 +10,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicioEditComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup; 
+  id: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private servSrv: ServicioControllerService,
+    private activeRoute: ActivatedRoute) {
+      this.buildForm()
+     }
 
   ngOnInit(): void {
+
+    this.activeRoute.params.subscribe(
+      (params: Params)=> {
+        this.id = params.id;
+        this.servSrv.findIdUsingGET(this.id).subscribe(servicio => {
+          this.form.patchValue(servicio);
+        } )
+        });
   }
+
+
+  saveProduct(event: Event){
+    event.preventDefault();
+    if(this.form.valid){
+      const servicio = this.form.value;
+      this.servSrv.editarServicioUsingPUT(servicio)
+      .subscribe((newServicio)=>{
+        console.log(newServicio);
+        this.router.navigate(['./admin/servicios-list']);
+      }
+      )
+    }
+    console.log(this.form.value);
+  }
+
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      nombre: ['', [Validators.required]],
+      descripcion: ['', [Validators.required]],
+      precio: ['', [Validators.required]],
+    })
+  }
+
+
+
+
+
 
 }
